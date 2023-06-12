84fbc51e0a33f4f3837d133d85b9c31291d48cfa
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-12 00:31:40 -0700

    Updated table and database

--- db/schema.sql
+++ db/schema.sql
@@ -13,6 +13,10 @@ CREATE TABLE role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
+
+    FOREIGN KEY(department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
