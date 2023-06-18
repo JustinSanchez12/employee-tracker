c9b265cf21082a0cd1cb8008435ebed8c65807fc
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-18 15:42:49 -0700

    updating prompt, testing the actions with View all Employees

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 
 class CLI {
+  constructor(db){
+    this.db = db;
+  }
   run() {
     return inquirer
       .prompt([
@@ -12,13 +15,47 @@ class CLI {
         },
       ])
       .then((answers) => {
-        console.info(answers.employee);
+        const { employees } = answers;
+        switch(employees) {
+          case 'View All Employees':
+            this.viewAllEmployees();
+            break;
+        }
       })
       .catch((err) => {
         console.log(err);
         console.log('Oops. Something went wrong.');
       });
   }
+  viewAllEmployees() {
+    const query = `
+      SELECT 
+        employee.id,
+        employee.first_name,
+        employee.last_name,
+        role.title,
+        department.name AS department,
+        role.salary,
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM 
+        employee
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON role.department_id = department.id
+        LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+    `;
+    this.db.query(query, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.table(results);
+      this.run();
+    });
+  };
 }
+
+
+
+  
 
 module.exports = CLI;
